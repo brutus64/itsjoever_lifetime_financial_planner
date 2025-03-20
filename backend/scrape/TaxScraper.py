7db664d11deff1a6f0e6bfce8191f25380a36aac
Edwin Cheng <edwincheng4840@gmail.com>
2025-03-19 23:34:25 -0400

    scrape capital gains

--- backend/scrape/TaxScraper.py
+++ backend/scrape/TaxScraper.py
@@ -4,7 +4,10 @@ from bs4 import BeautifulSoup
 class TaxScraper:
     '''
         Returns:
-            [single, married] 
+            { 
+                'single': single, 
+                'married': married
+            } 
                 - where single/married are lists whose elements take the form:
             (minIncome: Float, maxIncome: Float, rate: Float)
 
@@ -33,13 +36,76 @@ class TaxScraper:
 
         return [parse_single_married(tax_table[0]), parse_single_married(tax_table[1])]
 
-    def scrape_state_income(self):
-        pass
+    '''
+        Returns:
+            { 
+                'single': single, 
+                'married': married
+            } 
+                - where single/married are lists whose elements take the form:
+            (minIncome: Float, maxIncome: Float, rate: Float)
+
+        Note: A value of -1.0 is used to represent "And above" for the last bracket's  maxIncome 
+    '''
+    def scrape_capital_gains(self):
+        url = 'https://www.irs.gov/taxtopics/tc409'
+        response = requests.get(url)
+
+        soup = BeautifulSoup(response.content, 'lxml')
+        p_tags = soup.find_all('p')
+
+        percentages = []
+        for p_tag in p_tags:
+            if 'capital gains rate of' in p_tag.text:
+                text = p_tag.text
+                percent_string = text.split("capital gains rate of")[1]
+                percent_index = percent_string.find('%')
+                percent = float(percent_string[:percent_index].strip())
+                percentages.append(percent)
+
+        def find_capital_gains(text):
+            space_index = text.find(' ')
+            val = float(text[:space_index].replace('$', '').replace(',', ''))
+            return val
+
+        single_range = []
+        married_range = []
+        def scrape_helper(ul_tag, zero_or_fifteen):
+            li_tags = ul_tag.find_all('li')
+            single_tax_rate = li_tags[0].text
+            married_tax_rate = li_tags[1].text
+            if zero_or_fifteen == 0:
+                single_range.append([0, find_capital_gains(single_tax_rate)])
+                married_range.append([0, find_capital_gains(married_tax_rate)])
+            elif zero_or_fifteen == 15:
+                split_single = single_tax_rate.split("$")
+                split_married = married_tax_rate.split("$")
+
+                single_range.append([find_capital_gains(split_single[1])+1, find_capital_gains(split_single[2])])
+                married_range.append([find_capital_gains(split_married[1])+1, find_capital_gains(split_married[2])])
+
+
+                single_range.append([find_capital_gains(split_single[2])+1, -1])
+                married_range.append([find_capital_gains(split_married[2])+1, -1])
+
+        ul_container = soup.find_all('div', class_='field field--name-body field--type-text-with-summary field--label-hidden field--item')
+        ul_tags = ul_container[1].find_all('ul')
+        scrape_helper(ul_tags[0], 0)
+        scrape_helper(ul_tags[1], 15)
+
+        single = list(zip(*zip(*single_range), percentages))
+        married = list(zip(*zip(*married_range), percentages))
+
+        return {'single': single, 'married': married}
+
 
 
     '''
         Returns:
-            [single: Float, married: Float]
+            {
+                'single': Float, 
+                'married': Float
+            }
     '''
     def scrape_standard_deductions(self):
         url = 'https://www.irs.gov/publications/p17'
@@ -64,9 +130,10 @@ class TaxScraper:
             amount = float(cells[1].text.strip().replace(',', '').replace('$', ''))
             standard_deductions.append(amount)
         
-        return standard_deductions
+        return {'single': standard_deductions[0], 'married': standard_deductions[1]}
 
 
 test = TaxScraper()
-print(test.scrape_standard_deductions())
+# print(test.scrape_standard_deductions())
 # print(test.scrape_federal_income())
+print(test.scrape_capital_gains())
