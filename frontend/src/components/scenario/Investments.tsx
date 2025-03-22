cf4f23ff9fb5ac93257916cf8083a488db2775af
Ethan <ethan.b.chan@stonybrook.edu>
2025-03-22 15:52:22 -0400

    maininfo layout

--- frontend/src/components/scenario/Investments.tsx
+++ frontend/src/components/scenario/Investments.tsx
@@ -1,7 +1,73 @@
 const Investments = ({formData,setFormData}:any) => {
     return (
-        <div>
-            Investments
+        <div className="flex flex-col gap-4 m-10">
+            <div className="bg-white shadow-md rounded-lg p-6 flex flex-col gap-3 w-200">
+                <h1 className="text-2xl font-bold">Investments</h1>
+                <p>An investment is the allocation of money into assets such as stocks, bonds, or real estate, with the expectation that the value of those assets will grow over time, providing returns through appreciation, dividends, or interest payments.</p>
+            </div>
+            <div className="flex gap-10">
+                <div className="bg-white shadow-md rounded-lg p-6 flex flex-col flex-1 gap-3">
+                    <div className="flex justify-between">
+                        <h1 className="text-2xl font-bold">About You</h1>
+                        <div className="flex gap-5 align-middle">
+                            <h2 className="font-medium self-cener">Married:</h2>
+                            <input type="checkbox" name="is-married"/>  
+                        </div>
+                    </div>
+                    
+                    <div className="flex gap-5 align-middle">
+                        <h2 className="font-medium">Date of birth:</h2>
+                        <input type="date" className="text-md px-1 border-2 border-gray-200 rounded-md w-32" />
+                    </div>
+                    <div className="flex flex-col gap-3">
+                        <h2 className="font-medium">Life Expectancy (in years):</h2>
+                        <div className="flex gap-2 align-middle">
+                            <input className="ml-1" type="radio" name="user-exp"/>
+                            <div className="">Fixed:</div>
+                            <input className="text-md px-1 border-2 border-gray-200 rounded-md w-14" type="number" min="1"/> 
+                        </div>
+                        
+                        <div className="flex gap-2 align-middle">
+                            <input className="ml-1" type="radio" name="user-exp"/>
+                            <div className="">Normal:</div>
+                            <div className="">Mean:</div>
+                            <input className="text-md px-1 border-2 border-gray-200 rounded-md w-14" type="number" min="1"/> 
+                            <div className="">Variance:</div>
+                            <input className="text-md px-1 border-2 border-gray-200 rounded-md w-14" type="number" min="1"/> 
+                            
+                        </div>
+                    </div>
+                    
+                </div>
+                <div className="bg-white shadow-md rounded-lg p-6 flex flex-col flex-1 gap-3">
+                    <h1 className="text-2xl font-bold">Spouse</h1>
+                    <div className="flex gap-5 align-middle">
+                        <h2 className="font-medium">Date of birth:</h2>
+                        <input type="date" className="text-md px-1 border-2 border-gray-200 rounded-md w-32" />
+                    </div>
+                    <div className="flex flex-col gap-3">
+                        <h2 className="font-medium">Life Expectancy (in years):</h2>
+                        <div className="flex gap-2 align-middle">
+                            <input className="ml-1" type="radio" name="user-exp"/>
+                            <div className="">Fixed:</div>
+                            <input className="text-md px-1 border-2 border-gray-200 rounded-md w-14" type="number" min="1"/> 
+                        </div>
+                        
+                        <div className="flex gap-2 align-middle">
+                            <input className="ml-1" type="radio" name="user-exp"/>
+                            <div className="">Normal:</div>
+                            <div className="">Mean:</div>
+
+                            <input className="text-md px-1 border-2 border-gray-200 rounded-md w-14" type="number" min="1"/> 
+                            <div className="">Variance:</div>
+                            <input className="text-md px-1 border-2 border-gray-200 rounded-md w-14" type="number" min="1"/> 
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            
         </div>
     )
 }
--- frontend/src/components/scenario/ScenarioForm.tsx
+++ frontend/src/components/scenario/ScenarioForm.tsx
@@ -32,12 +32,15 @@ const ScenarioForm = ({scenario}:any) => {
     ];
     
     return (
-        <div>
+        <div className="flex flex-col justify-center align-middle">
             {/* Progess indicator here */}
-            {pages[currentPage-1]}
-            <div className="flex">
-                <button onClick={()=>setCurrentPage(currentPage-1)} disabled={currentPage==1}>Prev</button>
-                <button onClick={()=>setCurrentPage(currentPage+1)} disabled={currentPage==pages.length}>Next</button>
+            <div className="h-150">
+                {pages[currentPage-1]}
+            </div>
+            
+            <div className="flex justify-center gap-4">
+                <button className="flex bg-black text-white px-4 py-1 rounded-md hover:opacity-80 cursor-pointer disabled:opacity-20 disabled:cursor-default" onClick={()=>setCurrentPage(currentPage-1)} disabled={currentPage==1}>Prev</button>
+                <button className="flex bg-black text-white px-4 py-1 rounded-md hover:opacity-80 cursor-pointer disabled:opacity-20 disabled:cursor-default" onClick={()=>setCurrentPage(currentPage+1)} disabled={currentPage==pages.length}>Next</button>
             </div>
         </div>
     )
