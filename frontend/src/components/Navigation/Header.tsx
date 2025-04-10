8d9153445ca53a19956c56da7f7c7fb73938250a
Ethan <ethan.b.chan@stonybrook.edu>
2025-04-10 01:10:50 -0400

    made header display name, but useAuth doesnt seem to be working properly

--- frontend/src/components/Navigation/Header.tsx
+++ frontend/src/components/Navigation/Header.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { useAuth } from './AuthContext';
 
-const Header: React.FC<{ userInfo: any, toggleMenu: () => void }> = ({ userInfo, toggleMenu }) => {
+const Header: React.FC<{ toggleMenu: () => void }> = ({ toggleMenu }) => {
+    const { isLoggedIn, userInfo } = useAuth();
+    console.log(userInfo)
+    console.log(isLoggedIn)
     return (
         <div className='flex justify-between bg-white shadow-md'>
             <div className="flex items-center w-50 gap-5 p-4 hover:bg-gray-300 cursor-pointer" onClick={toggleMenu}>
                 <img className="w-6 h-6" src='/menu_icons/menu.png' alt='menu icon' />
                 <div className="flex-1 flex items-center">
-                    <span>{userInfo?.given_name ? `${userInfo.given_name} ${userInfo.family_name || ""}` : "Loading..."}</span>
+                    <span>{isLoggedIn ? userInfo?.name : "Guest"}</span>
                 </div>
             </div>
             <div className='bg-black px-4  m-4 text-lg text-white rounded-md hover:bg-gray-800'>
