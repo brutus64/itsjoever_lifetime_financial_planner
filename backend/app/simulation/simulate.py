55ff046b0372415bc7ab1f1b829159ded6e88f62
Ethan <ethan.b.chan@stonybrook.edu>
2025-04-12 00:56:51 -0400

    invest and expense objects

--- backend/app/simulation/simulate.py
+++ backend/app/simulation/simulate.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from statistics import mean, median
 import os
 import sys
+import math
 # from app.models.tax import StateTax, FederalTax, CapitalGains, RMDTable, StandardDeduct
 # from app.models.simulation import Simulation
 
@@ -50,6 +51,7 @@ class Vary:
 # that would not be in the scenario object
 class Investment:
     def __init__(self,investment):
+        self.id = investment["id"] # just in case
         self.value = investment["value"]
         self.purchase = investment["value"] # this is the amount of money put into the investment
         self.tax_status = investment["tax_status"]
@@ -85,27 +87,76 @@ class Investment:
         self.value -= avg * self.expense_ratio
         
         return inc
-        
-        
-
+    
+# note: for all event series, there is no need to store the start year or duration
+# as a Vary object since it is only sampled once in the beginning.
+# This is also the case for life expectancy
         
 # income event series
 class Income:
     def __init__(self,event_series):
-        pass
+        self.id = event_series["id"] # just in case
+        self.amt = event_series["details"]["initial_amt"]
+        self.exp_change = Vary(event_series["details"]["exp_annual_change"])
+        self.exp_change_percent = event_series["details"]["exp_annual_change"]["is_percent"]
+        self.inflation_adjust = event_series["details"]["inflation_adjust"]
+        self.user_split = event_series["details"]["user_split"]
+        self.social_security = event_series["details"]["social_security"]
+        self.name = event_series["name"]
+        self.start = event_series["start"] #resolved later
+        self.duration = max(0,math.floor(0.5+Vary(event_series["duration"]).generate())) # round to nearest integer, cannot be negative
+
+# expense event seriess
+class Expense:
+    def __init__(self,event_series):
+        self.id = event_series["id"] # just in case
+        self.amt = event_series["details"]["initial_amt"]
+        self.exp_change = Vary(event_series["details"]["exp_annual_change"])
+        self.exp_change_percent = event_series["details"]["exp_annual_change"]["is_percent"]
+        self.inflation_adjust = event_series["details"]["inflation_adjust"]
+        self.user_split = event_series["details"]["user_split"]
+        self.is_discretionary = event_series["details"]["is_discretionary"]
+        self.name = event_series["name"]
+        self.start = event_series["start"] #resolved later
+        self.duration = max(0,math.floor(0.5+Vary(event_series["duration"]).generate())) # round to nearest integer, cannot be negative
 
 class Tax: # tax brackets
     pass
 
-# expense event series
-class Expense:
-    
+class Invest:
+    pass
+
+class Rebalance:
+    pass
 
 # store simulation state
 class Simulation:
     def __init__(self,scenario):
         self.investments = [Investment(investment) for investment in scenario.get("investment")]
+        event_series = [] # only used here
+        self.income = []
         self.expenses = []
+        for es in scenario.get("event_series"):
+            t = es.get("type")
+            if t == "income":
+                inc = Income(es)
+                event_series.append(inc)
+                self.income.append(inc)
+            elif t == "expense":
+                exp = Expense(es)
+                event_series.append(exp)
+                self.expenses.append(exp)
+            elif t == "invest":
+                inv = Invest(es)
+                event_series.append(inv)
+                self.invest_strat.append(inv)
+            elif t == "rebalance":
+                re = Rebalance(es)
+                event_series.append(re)
+                self.rebalance.append(re)
+        resolve_event_start(event_series)
+
+        # places that require Investment objects: invest/rebalance event series, strategies, 
         
 
 # data for a particular year in a simulation
@@ -121,6 +172,34 @@ class YearlyResults:
         self.total_expenses = 0 # includes expense event series and taxes
         self.early_withdrawal_tax = 0 
         self.discretionary_percent = 0 # discretionary expenses paid / total discretionary
+
+# dfs to get a fixed year for event start times
+def resolve_event_start(event_series):
+    visited = set() # visited during dfs, but may not be resolved
+    resolved = set()
+    def dfs(es):
+        if es in resolved: # Start date already resolved
+            return es.start 
+        if es in visited: # Cycle detected, set to default
+            es.start = 2025
+            resolved.add(es.id)
+            return 2025 
+        visited.add(es.id)
+        if es.start["type"] == "start_with": 
+            es.start = dfs(es.start["event_series"])
+        elif es.start["type"] == "end_with":
+            es.start = dfs(es.start["event_series"]) + 1
+        else:
+            es.start = math.floor(0.5+Vary(es.start).generate())
+        resolved.add(es.id)
+        return es.start
+
+    for es in event_series:
+        if es.start["type"] == "start_with" or es.start["type"] == "end_with":
+            dfs(es)
+        else:
+            es.start
+
 
 
 # set of n simulations
@@ -193,4 +272,5 @@ def simulate_log(scenario,tax_data,user):
 
 # testing
 if __name__ == "__main__":
-    simulate_n({},10000,"Jeff")
+    vary = Vary({"type":"uniform","mean":4,"stdev":5,"value":17,"lower":19,"upper":21})
+    print(vary.generate())
